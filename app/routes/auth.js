909930e08497ac2c1526f2df3810dd482de494cc
kAVArd <savinovilja1999@gmail.com>
2019-08-12 17:00:20 +0300

    add user schema with bcrypting password

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -1,8 +1,7 @@
 const authRouter = require('express').Router()
+const passport = require('passport')
 
-authRouter.route('/login/google').get((req, res) => {
-  res.send('auth with passport google')
-})
+authRouter.route('/login/google').get()
 
 authRouter.route('/logout').get((req, res) => {
   res.send('log out')
--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 const express = require('express')
 require('dotenv').config()
 const bodyParser = require('body-parser')
-const app = express()
 const mongoose = require('mongoose')
 const cors = require('cors')
+const routes = require('./app/routes')
+const passportSetup = require('./config/passport-setup')
 
+const app = express()
 const port = 8000
 
+// config mongo
 mongoose.connect(`${process.env.MONGODB_URL}`, { useNewUrlParser: true })
 const connection = mongoose.connection
 connection.once('open', function () {
   console.log('MongoDB database connection established successfully')
 })
 
+// express app configs
 app.listen(port, () => console.log(`Backend work on port ${port}`))
 app.use(bodyParser.json())
 app.use(cors())
 
-require('./app/routes')(app)
+routes(app)
